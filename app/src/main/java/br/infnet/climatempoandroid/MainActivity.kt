9732572f7ef10313feddb15a5f38faaa26337e11
Ruan Campelo Neri <raidruan@gmail.com>
2022-11-11 22:26:12 -0300

    desfazer ateracao

--- app/src/main/java/br/infnet/climatempoandroid/MainActivity.kt
+++ app/src/main/java/br/infnet/climatempoandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package br.infnet.climatempoandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.infnet.climatempo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
